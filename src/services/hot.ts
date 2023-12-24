ff4e72b63c76d587c36deec083a73bd1dd56ce61
青芥 <54880754@qq.com>
2023-12-24 20:47:25 +0800

    feat: 热门推荐类型定义

--- src/services/hot.ts
+++ src/services/hot.ts
@@ -1,13 +1,15 @@
 import type { PageParams } from '@/types/global'
+import type { HotItem } from '@/types/home'
 import { http } from '@/utils/http'
 
 /* 通用推荐类型
   @params url 请求地址
   @params data 请求参数
 */
+// &表示交叉类型，交叉类型可以基于原来的类型做一个扩展
 type HotParams = PageParams & { subType?: string }
 export const getHotRecommendAPI = (url: string, data?: HotParams) => {
-  return http({
+  return http<HotItem>({
     method: 'GET',
     url,
     data,
