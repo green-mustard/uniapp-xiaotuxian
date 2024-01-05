34dcb8e05f308dc4606e6c281f0d460f8752db43
青芥 <54880754@qq.com>
2024-01-05 22:39:44 +0800

    feat: 地址模块-新建地址

---
+++ src/services/address.ts
@@ -0,0 +1,13 @@
+/* 地址相关的API */
+
+import type { AddressParams } from '@/types/address'
+import { http } from '@/utils/http'
+
+// 添加收货地址
+export const postMemberAddressAPI = (data: AddressParams) => {
+  return http({
+    method: 'POST',
+    url: '/member/address',
+    data,
+  })
+}
