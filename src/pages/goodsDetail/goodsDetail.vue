bd346ed267d17d5b19916977d8f7ce0c49d47778
青芥 <54880754@qq.com>
2024-01-12 15:52:04 +0800

    feat: 购物车模块-购物车抽离成单独组件以及设置两个购物车页面

--- src/pages/goodsDetail/goodsDetail.vue
+++ src/pages/goodsDetail/goodsDetail.vue
@@ -240,7 +240,7 @@ const onAddCart = async (event: SkuPopupEvent) => {
       <button class="icons-button" open-type="contact">
         <text class="icon-handset"></text>客服
       </button>
-      <navigator class="icons-button" url="/pages/cart/cart" open-type="switchTab">
+      <navigator class="icons-button" url="/pages/cart/cartcopy" open-type="navigate">
         <text class="icon-cart"></text>购物车
       </navigator>
     </view>
